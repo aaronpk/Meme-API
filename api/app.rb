da2d248d74193065f17150294fba19dfd842f802
Loqi <dev@geoloqi.com>
2014-03-02 18:39:56 +0000

    gif inputs result in gif outputs (makes animaged gifs work!!)

--- api/app.rb
+++ api/app.rb
@@ -8,7 +8,12 @@ get '/g' do
   open(params[:u], 'rb') do |f|
     begin
       i = MemeCaptain.meme_top_bottom(f, params[:t1], params[:t2], :font => './Impact.ttf')
-      filename = "#{DateTime.now.to_sxg}#{SecureRandom.random_number(60**5).to_sxg}.jpg"
+      if(params[:u].match(/\.gif$/))
+        ext = "gif"
+      else 
+        ext = "jpg"
+      end
+      filename = "#{DateTime.now.to_sxg}#{SecureRandom.random_number(60**5).to_sxg}.#{ext}"
       i.write("./public/m/#{filename}")
       headers 'Content-type' => 'application/json'
       {imageUrl: "http://#{request.host}/#{filename}"}.to_json
